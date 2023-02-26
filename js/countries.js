32e345315ea166e00f0f0d4cfa4f82358d1a8841
Md-Azad <sokalazad@gmail.com>
2023-02-26 14:53:40 +0100

    rest countries api added and shown country name and flag

--- js/countries.js
+++ js/countries.js
@@ -8,19 +8,42 @@ const loadCountirs = ()=>{
 const displayCountries = countries =>{
     // console.log(countries[0].name.common);
     countries.forEach(country=>{
+        // console.log(country.cca2)
 
         
         const countriesDiv = document.getElementById('countries');
 
+            // console.log(country.cca2)
         const makeDiv = document.createElement('div');
         makeDiv.classList.add('country');
         makeDiv.innerHTML = `
-            <h1>Country name: ${country.name.common} </h1>
+            <h1>country name: ${country.name.common} </h1>
             <p> Capital : ${country.capital ? country.capital:"no Capital"} </p>
+            <button onclick = loadCountriesDetails('${country.cca2}')> Details</button> 
+           
         
         `
         countriesDiv.appendChild(makeDiv);
     })
+}
+const loadCountriesDetails = code =>{
+    // console.log("details are coming",code)
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log(url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountriesDetails(data[0]))
+}
+const showCountriesDetails = country =>{
+    console.log(country);
+    const countryLocation = document.getElementById('details');
+    countryLocation.innerHTML = `
+        <h2> Name: ${country.name.common}  </h2>
+        <img src ='${country.flags.png}'>
+        
+        `
+
 }
 
 loadCountirs();
